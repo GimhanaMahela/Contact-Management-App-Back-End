429c3956e8139ffb9432ffe17802af4558a956c4
Gimhan Mahela <gimhanmahela@gmail.com>
2024-02-06 12:45:56 +0530

    Contact Management Application

---
+++ mycontacts-backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { loginUser, currentUser, registerUser } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/current", validateToken ,currentUser);
+
+module.exports= router;
